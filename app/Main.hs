2fe3f6a9633aaebb38d22cf98b709f8ab1bb627d
Matthew Leon <ml@matthewleon.com>
2017-08-21 11:43:17 +0100

    error recovery

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@
 
 module Main (main) where
 
-import Control.Exception.Safe (throw, throwString)
+import Control.Exception.Safe (throw, throwString, catchAny)
 import Music.MusicBrainz (searchSong)
 import Network.MPD (MPD, Song(sgTags), Subsystem(..), withMPD, currentSong, idle, playlistInfo)
 import SDL hiding (Debug)
@@ -52,8 +52,8 @@ main = do
   info "starting"
   getCurrentSongCover Nothing >>= initSystems
 
-initSystems :: (Song, JPEG) -> IO ()
-initSystems initSongCover@(_, initJpeg) =
+initSystems :: Maybe (Song, JPEG) -> IO ()
+initSystems maybeInitSongCover =
   initializeAll >> SystemChange.register >>= \case
     Nothing -> throwString "Unable to register user event with SDL."
     Just (RegisteredEventType pushSystemChangeEvent getSystemChangeEvent) -> do
@@ -63,8 +63,8 @@ initSystems initSongCover@(_, initJpeg) =
       setHintWithPriority OverridePriority HintRenderScaleQuality ScaleLinear
       window <- createWindow title windowConfig
       renderer <- createRenderer window (-1) defaultRenderer
-      watch <- update renderer Nothing initJpeg
-      appLoop renderer watch getSystemChangeEvent initSongCover
+      watch <- update renderer Nothing (snd <$> maybeInitSongCover)
+      appLoop renderer watch getSystemChangeEvent maybeInitSongCover
 
 mpdThread :: (SystemChangeEvent -> IO EventPushResult) -> IO ()
 mpdThread push = go
@@ -75,11 +75,11 @@ mpdThread push = go
       EventPushFiltered -> throwString "System change event filtered."
       EventPushFailure s -> throwString $ T.unpack s
 
-appLoop :: Renderer -> EventWatch -> (Event -> IO (Maybe SystemChangeEvent)) -> (Song, JPEG) -> IO ()
+appLoop :: Renderer -> Maybe EventWatch -> (Event -> IO (Maybe SystemChangeEvent)) -> Maybe (Song, JPEG) -> IO ()
 appLoop renderer watch getSystemChangeEvent initSongCover =
   waitEvent >>= go watch initSongCover
   where
-  go :: EventWatch -> (Song, JPEG) -> Event -> IO ()
+  go :: Maybe EventWatch -> Maybe (Song, JPEG) -> Event -> IO ()
   go watch previousSongCover ev = case eventPayload ev of
      KeyboardEvent keyboardEvent
        |  keyboardEventKeyMotion keyboardEvent == Pressed &&
@@ -88,45 +88,56 @@ appLoop renderer watch getSystemChangeEvent initSongCover =
      _ ->
       getSystemChangeEvent ev >>= \case
         Just systemChangeEvent -> do
-          songCover <- getCurrentSongCover $ Just previousSongCover
-          watch' <- update renderer (Just watch) $ snd songCover
-          waitEvent >>= go watch songCover
+          songCover <- getCurrentSongCover previousSongCover
+          watch' <- update renderer watch $ snd <$> songCover
+          waitEvent >>= go watch' songCover
         Nothing -> waitEvent >>= go watch previousSongCover
 
-update :: Renderer -> Maybe EventWatch -> JPEG -> IO EventWatch
-update renderer maybeWatch (JPEG cover) = do
-  texture <- decodeTexture renderer cover
-  draw renderer texture
+update :: Renderer -> Maybe EventWatch -> Maybe JPEG -> IO (Maybe EventWatch)
+update renderer maybeWatch maybeCover = do
   maybe (pure ()) delEventWatch maybeWatch
-  addEventWatch $ resizeWatch renderer texture
+  case maybeCover of
+    Just (JPEG cover) -> do
+      texture <- decodeTexture renderer cover
+      draw renderer texture
+      Just <$> addEventWatch (resizeWatch renderer texture)
+    Nothing -> pure maybeWatch
 
-getCurrentSongCover :: Maybe (Song, JPEG) -> IO (Song, JPEG)
+getCurrentSongCover :: Maybe (Song, JPEG) -> IO (Maybe (Song, JPEG))
 getCurrentSongCover maybePrevious = do
   currentSong <- getCurrentSong
   case maybePrevious of
     Just previous@(previousSong, _)
       |  previousSong == currentSong
-      -> pure previous
-    _ -> (currentSong,) <$> getCover currentSong
+      -> pure $ Just previous
+    _ -> Just . (currentSong,) <$> getCover currentSong
+  `catchAny` \e -> do
+    info $ "unable to obtain album art: " <> T.pack (displayException e)
+    pure Nothing
+
+newtype SongFetchException = SongFetchException Text
+  deriving (Show, Typeable)
+instance Exception SongFetchException where
+  displayException (SongFetchException t) = T.unpack t
 
 getCurrentSong :: IO Song
 getCurrentSong = do
   debug "querying mpd for current song"
-  song <- throwOnNothing "no current song" =<< withMPD' currentSong
+  song <- throwFetchOnNothing "no current song" =<< withMPD' currentSong
   debug $ "current song: " <> show song
   pure song
 
 getCover :: Song -> IO JPEG
 getCover song = do
   debug "fetching mbid"
-  mbid <- throwOnNothing "unable to find mbid for song" =<< searchSong song
+  mbid <- throwFetchOnNothing "unable to find mbid for song" =<< searchSong song
   debug $ "mbid: " <> show mbid
   debug "fetching art"
-  jpeg <- throwOnLeft "unable to get album art for mbid" =<< CAA.getFront mbid
+  jpeg <- throwFetchOnLeft "unable to get album art for mbid" =<< CAA.getFront mbid
   debug "fetched art"
   pure jpeg
   where
-  throwOnLeft str = either (const $ throwString str) return
+  throwFetchOnLeft t = either (const $ throwFetch t) return
 
 draw :: Renderer -> Texture -> IO ()
 draw renderer texture = do
@@ -160,5 +171,11 @@ centerOrigin (V2 smallerw smallerh) (V2 largerw largerh) =
 withMPD' :: MPD a -> IO a
 withMPD' m = withMPD m >>= either throw return
 
-throwOnNothing :: String -> Maybe a -> IO a
-throwOnNothing str = maybe (throwString str) return
+throwOnNothing :: Exception e => e -> Maybe a -> IO a
+throwOnNothing e = maybe (throw e) return
+
+throwFetchOnNothing :: Text -> Maybe a -> IO a
+throwFetchOnNothing t = throwOnNothing (SongFetchException t)
+
+throwFetch :: Text -> IO a
+throwFetch = throw . SongFetchException
